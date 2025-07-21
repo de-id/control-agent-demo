87accf52572f091371042813c40dcb64e340ed4f
Dor Eitan <dor.eitan@deidentification.co>
2025-07-21 21:49:05 +0300

    integrate interrupt button functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,29 @@
 import { ChatMode } from "@d-id/client-sdk";
 import type { Configurations, Props } from "@d-id/agent-client";
 import { getAuth, init, testMode } from "@d-id/agent-client";
-import { startButtonCreation } from "./buttonManager";
+import { observeInterruptButton } from "./interrupt-button";
 
 const scriptTag = document.querySelector('script[data-name="did-agent"]')!;
 
-const params = new URLSearchParams(testMode === "true" ? window.location.search : "");
+const params = new URLSearchParams(
+  testMode === "true" ? window.location.search : ""
+);
 
-const mode = (params.get("mode") || (scriptTag.getAttribute("data-mode") ?? "fabio")) as
-  | "fabio"
-  | "full";
+const mode = (params.get("mode") ||
+  (scriptTag.getAttribute("data-mode") ?? "fabio")) as "fabio" | "full";
 const target = scriptTag.getAttribute("data-target-id");
-const agentId = params.get("agent_id") || scriptTag.getAttribute("data-agent-id");
+const agentId =
+  params.get("agent_id") || scriptTag.getAttribute("data-agent-id");
 const didApiUrl = scriptTag.getAttribute("data-api-url");
 const token = scriptTag.getAttribute("data-token");
 const username = scriptTag.getAttribute("data-username");
 const password = scriptTag.getAttribute("data-password");
-const clientKey = params.get("key") || scriptTag.getAttribute("data-client-key");
+const clientKey =
+  params.get("key") || scriptTag.getAttribute("data-client-key");
 const track = scriptTag.getAttribute("data-track") !== "false";
 const monitor = scriptTag.getAttribute("data-monitor") !== "false";
-const position = (scriptTag.getAttribute("data-position") ?? "right") as Configurations["position"];
+const position = (scriptTag.getAttribute("data-position") ??
+  "right") as Configurations["position"];
 const orientation = (scriptTag.getAttribute("data-orientation") ??
   "vertical") as Configurations["orientation"];
 
@@ -33,7 +37,7 @@ const props: Props = {
   callbacks: {
     onStreamCreated: (event) => {
       console.log("Stream created: ", event);
-      startButtonCreation();
+      observeInterruptButton();
     },
     onSttEnd: (event) => console.log("User speak result: ", event.result.text),
   },
