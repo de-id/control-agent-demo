377ea663a89f9eb0fd4688cf22997ca7f7daff5d
dariusz-did <dar.dudek@deidentification.co>
2025-07-21 20:51:00 +0200

    Feature/interruption button (#1)
    * removed sstProvided prop
    
    * added interruption button
    
    * integrate interrupt button functionality
    
    ---------
    
    Co-authored-by: Dor Eitan <dor.eitan@deidentification.co>

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,29 @@
 import { ChatMode } from "@d-id/client-sdk";
 import type { Configurations, Props } from "@d-id/agent-client";
 import { getAuth, init, testMode } from "@d-id/agent-client";
+import { observeInterruptButton } from "./interrupt-button";
 
 const scriptTag = document.querySelector('script[data-name="did-agent"]')!;
 
-const params = new URLSearchParams(testMode === "true" ? window.location.search : "");
+const params = new URLSearchParams(
+  testMode === "true" ? window.location.search : ""
+);
 
-const mode = (params.get("mode") || (scriptTag.getAttribute("data-mode") ?? "fabio")) as
-  | "fabio"
-  | "full";
+const mode = (params.get("mode") ||
+  (scriptTag.getAttribute("data-mode") ?? "fabio")) as "fabio" | "full";
 const target = scriptTag.getAttribute("data-target-id");
-const agentId = params.get("agent_id") || scriptTag.getAttribute("data-agent-id");
+const agentId =
+  params.get("agent_id") || scriptTag.getAttribute("data-agent-id");
 const didApiUrl = scriptTag.getAttribute("data-api-url");
 const token = scriptTag.getAttribute("data-token");
 const username = scriptTag.getAttribute("data-username");
 const password = scriptTag.getAttribute("data-password");
-const clientKey = params.get("key") || scriptTag.getAttribute("data-client-key");
+const clientKey =
+  params.get("key") || scriptTag.getAttribute("data-client-key");
 const track = scriptTag.getAttribute("data-track") !== "false";
 const monitor = scriptTag.getAttribute("data-monitor") !== "false";
-const position = (scriptTag.getAttribute("data-position") ?? "right") as Configurations["position"];
+const position = (scriptTag.getAttribute("data-position") ??
+  "right") as Configurations["position"];
 const orientation = (scriptTag.getAttribute("data-orientation") ??
   "vertical") as Configurations["orientation"];
 
@@ -30,10 +35,14 @@ const props: Props = {
   didApiUrl: didApiUrl!,
   monitor,
   callbacks: {
-    onStreamCreated: event => console.log("Stream created: ", event),
-    onSttEnd: event => console.log("User speak result: ", event.result.text)
-  }
+    onStreamCreated: (event) => {
+      console.log("Stream created: ", event);
+      observeInterruptButton();
+    },
+    onSttEnd: (event) => console.log("User speak result: ", event.result.text),
+  },
 };
+
 const configs: Partial<Configurations> = {
   mode,
   targetElement: target,
@@ -44,7 +53,6 @@ const configs: Partial<Configurations> = {
   position,
   orientation,
   showChatToggle: false,
-  sttProvider: "native"
 };
 
 init({ ...configs, ...props });
